5326ccb97ed8dcd2b444cb65fc0392b9b2ca1d74
Santiago Storti <santiago.storti@mercadolibre.com>
2015-07-15 16:58:09 -0300

    Commit Inicial

---
+++ grails-app/domain/com/test/Test1.groovy
@@ -0,0 +1,27 @@
+package com.test
+
+class Test1 {
+
+    String var1
+    int value2
+
+
+
+    static constraints = {
+    }
+
+
+    def metodoFeo() {
+        if (true) {
+            return "true"
+        }
+        else if (false) {
+            return "false"
+        }
+        else {
+            return "branch innecesario"
+        }
+    }
+
+
+}
---
+++ test/unit/com/test/Test1Tests.groovy
@@ -0,0 +1,17 @@
+package com.test
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Test1)
+class Test1Tests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
